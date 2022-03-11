bb86515aafe3fc5f924b834d9dc49090fbd2b493
Umk1nus <93904441+Umk1nus@users.noreply.github.com>
2022-03-11 14:57:36 +0300

    style header complete

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -2,14 +2,14 @@
   <header class="header">
     <img src="../assets/img/logo.svg" alt="" class="header__logo">
     <nav class="header__nav">
-      <a href="" class="nav-item">Главная</a>
+      <a href="" class="nav-item active">Главная</a>
       <a href="" class="nav-item">Магазин</a>
       <a href="" class="nav-item">Отзывы</a>
       <a href="" class="nav-item">О нас</a>
     </nav>
     <div class="header__lk">
       <button class="lk-autorisation">Войти</button>
-      <a href="" class="lk-backet"><img src="../assets/img/icon/basket.png" alt=""></a>
+      <a href="/" class="lk-basket"><img src="../assets/img/icon/basket.png" alt=""><span>1</span></a>
     </div>
   </header>
 </template>
