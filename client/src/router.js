479a9a5138bb1239d0f2c5a6be46f565138500a9
Umk1nus <93904441+Umk1nus@users.noreply.github.com>
2022-03-13 17:18:03 +0300

    footer template and css  1part complete

--- client/src/router.js
+++ client/src/router.js
@@ -3,6 +3,10 @@ import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
 const routerHistory = createWebHashHistory()
 
 import homePage from "@/pages/home.vue"
+import shopPage from "@/pages/shop.vue"
+import reviewsPage from "@/pages/reviews.vue"
+import aboutPage from "@/pages/about.vue"
+import notFoundPage from "@/pages/notFound.vue"
 
 
 
@@ -13,6 +17,26 @@ const routers = createRouter({
       path: '/',
       name: 'Home',
       component: homePage,
+    },
+    {
+      path: '/shop',
+      name: 'Shop',
+      component: shopPage,
+    },
+    {
+      path: '/reviews',
+      name: 'Reviews',
+      component: reviewsPage,
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: aboutPage,
+    },
+    {
+      path: '/:CatchAll(.*)',
+      name: '404',
+      component: notFoundPage,
     },
   ]
 })
