b3032f9ec02a576b2807d84d12c60ea2567b1908
Umk1nus <93904441+Umk1nus@users.noreply.github.com>
2022-03-25 16:58:40 +0300

    template review

---
+++ client/src/components/UI/Review.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="review">
+    <div class="review__content">
+      <div class="review__content-img">
+        <img src="@/img/avatar/crocodile.svg" alt="" class="review__content-img-avatar">
+      </div>
+      <h2 class="review__content-title">Даниил, <span>19</span></h2>
+      <div class="review__content-description">Очень быстрая доставка и качественный товар.
+        Буду заказывать еще.
+      </div>
+    </div>
+        <div class="review__content">
+      <div class="review__content-img">
+        <img src="@/img/avatar/crocodile.svg" alt="" class="review__content-img-avatar">
+      </div>
+      <h2 class="review__content-title">Даниил, <span>19</span></h2>
+      <div class="review__content-description">Очень быстрая доставка и качественный товар.
+        Буду заказывать еще.
+      </div>
+    </div>
+        <div class="review__content">
+      <div class="review__content-img">
+        <img src="@/img/avatar/crocodile.svg" alt="" class="review__content-img-avatar">
+      </div>
+      <h2 class="review__content-title">Даниил, <span>19</span></h2>
+      <div class="review__content-description">Очень быстрая доставка и качественный товар.
+        Буду заказывать еще.
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- client/src/pages/reviews.vue
+++ client/src/pages/reviews.vue
@@ -1,4 +1,22 @@
 <template>
-  <h1>Отзывы</h1>
+  <div class="reviews">
+    <Review/>
+  </div>
 </template>
 
+<script>
+import Review from "@/components/UI/Review.vue"
+import reviews from "@/seeders/reviews.js"
+export default {
+  components:{Review},
+  data() {
+    return {
+      reviews: reviews
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
