994152a04c1b9a5e2b811f5f302611e4d2d61ddc
Umk1nus <93904441+Umk1nus@users.noreply.github.com>
2022-05-22 18:08:31 +0300

    company block part 1 complete

--- client/src/pages/home.vue
+++ client/src/pages/home.vue
@@ -1,5 +1,62 @@
 <template>
-  <Store/>
+  <div class="home">
+    <div class="company">
+      <div class="up-company">
+        <div class="company-blocks">
+          <div class="company-blocks__block blue-bg">
+            <img src="@/assets/img/icon/cardCompany.svg" alt="" class="company-blocks__block-img">
+            <p class="company-blocks__block-title">
+              Оплата картой и наличными
+            </p>
+          </div>
+          <div class="company-blocks__block red-bg">
+            <img src="@/assets/img/icon/storeCompany.svg" alt="" class="company-blocks__block-img">
+            <p class="company-blocks__block-title">
+              Мы работаем круглосуточно
+            </p>
+          </div>
+          <div class="company-blocks__block green-bg">
+            <img src="@/assets/img/icon/mapCompany.svg" alt="" class="company-blocks__block-img">
+            <p class="company-blocks__block-title">
+              Отслеживайте ваш заказ
+            </p>
+          </div>
+          <div class="company-blocks__block red-bg">
+            <img src="@/assets/img/icon/packageCompany.svg" alt="" class="company-blocks__block-img">
+            <p class="company-blocks__block-title">
+              У нас быстрая доставка
+            </p>
+          </div>
+          <div class="company-blocks__block green-bg">
+            <img src="@/assets/img/icon/reviewCompany.svg" alt="" class="company-blocks__block-img">
+            <p class="company-blocks__block-title">
+              Оставляйте свои отзывы
+            </p>
+          </div>
+          <div class="company-blocks__block blue-bg">
+            <img src="@/assets/img/icon/boxCompany.svg" alt="" class="company-blocks__block-img">
+            <p class="company-blocks__block-title">
+              Вам доступны акции
+            </p>
+          </div>
+        </div>
+        <div class="company-info">
+          <h2 class="company-info__title">
+            Мы ради приветствовать Вас в интернет-Магазине нашей компании!
+          </h2>
+          <p class="company-info__content">
+            Наша  компания была основана в 2021 году,
+            занимается импортом и реализацией продуктов культуры из Японии
+          </p>
+          <button class="company-info__btn">Подробнее</button>
+        </div>
+        <div class="down-company">
+          
+        </div>
+      </div>
+    </div>
+    <Store/>
+  </div>
 </template>
 
 <script>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -10,15 +10,30 @@ export const store = createStore({
     ],
     managment: [
       { id: 1, photo: 'photo', fullName: 'Илья Дорогин', position: 'Генеральный директор'},
-      { id: 1, photo: 'photo', fullName: 'Владислав Иванов', position: 'Директор по развитию и франчайзингу'},
-      { id: 1, photo: 'photo', fullName: 'Антон Желудков', position: 'Директор по производству'},
-      { id: 1, photo: 'photo', fullName: 'Анна Назарова', position: 'Директор по маркетингу'},
-      { id: 1, photo: 'photo', fullName: 'Юлия Сидорова', position: 'Директор по доставке'},
-      { id: 1, photo: 'photo', fullName: 'Михаил Милютин', position: 'Финансовый директор'}
+      { id: 2, photo: 'photo', fullName: 'Владислав Иванов', position: 'Директор по развитию и франчайзингу'},
+      { id: 3, photo: 'photo', fullName: 'Антон Желудков', position: 'Директор по производству'},
+      { id: 4, photo: 'photo', fullName: 'Анна Назарова', position: 'Директор по маркетингу'},
+      { id: 5, photo: 'photo', fullName: 'Юлия Сидорова', position: 'Директор по доставке'},
+      { id: 6, photo: 'photo', fullName: 'Михаил Милютин', position: 'Финансовый директор'}
     ],
+    cards: [
+      { id: 1, title: 'Стикеры Астольфо', alias: 'astolfo', price: '400 руб', img: 'src/assets/img/product/astolfocard.png' },
+      { id: 2, title: 'Фигурка Ацуши', alias: 'acushi', price: '900 руб', img: 'src/assets/img/product/acushicard.png'},
+      { id: 3, title: 'Рамен Токусима', alias: 'tokusima', price: '200 руб', img: 'src/assets/img/product/tokusimacard.png'},
+      { id: 4, title: 'Рамен Суисан', alias: 'suisan', price: '200 руб', img: 'src/assets/img/product/suisancard.png'},
+      { id: 5, title: 'Стикеры 02', alias: '02', price: '400 руб', img: 'src/assets/img/product/02card.png'},
+      { id: 6, title: 'Стикеры Ахегао', alias: 'ahegao', price: '300 руб', img: 'src/assets/img/product/ahegaocard.png'},
+    ]
 
   },
-  getters: {},
+  getters: {
+    getReviews(state) {
+      return state.reviews
+    },
+    getManagment(state) {
+      return state.managment
+    }
+  },
   mutations: {},
   actions: {},
 })
